8db235ad226b0834db92efe59b7850e6cc6a80c2
David <david.svartz@pija.se>
2019-11-20 15:37:12 +0100

    Dark theme.

--- packages/testbed/src/components/layout/Sidebar.js
+++ packages/testbed/src/components/layout/Sidebar.js
@@ -5,44 +5,50 @@ type Props = {
   active: boolean,
 };
 
-const links = [
-  'Overview',
-  'Resources',
-  'Quality',
-  'Volumes',
-  'Leadership',
-  'Statistics',
-  'Realtime',
-];
+const Sidebar = ({ active }: Props) => {
+  const links = [
+    'Overview',
+    'Resources',
+    'Quality',
+    'Volumes',
+    'Leadership',
+    'Statistics',
+    'Realtime',
+  ];
 
-const accountLinks = [
-  'Settings',
-  'Logout',
-];
+  const accountLinks = [
+    'Settings',
+    'Logout',
+  ];
 
-const Sidebar = ({ active }: Props) => (
-  <div
-    className={[
-      'site-sidebar',
-      active && 'site-sidebar_active',
-    ]
-      .filter(className => className)
-      .join(' ')}
-  >
-    <nav className="site-sidebar-menu">
-      {links.map(link => (
-        <div className="site-sidebar-menu-item" key={link}>
-          <a href={`/${link.toLowerCase()}`} className="button">{link}</a>
-        </div>
-      ))}
-      <div className="site-sidebar-menu-item-space" />
-      {accountLinks.map(link => (
-        <div className="site-sidebar-menu-item" key={link}>
-          <a href={`/${link.toLowerCase()}`} className="button">{link}</a>
-        </div>
-      ))}
-    </nav>
-  </div>
-);
+  const toggleTheme = () => document.body.classList.toggle('body_theme-dark');
+
+  return (
+    <div
+      className={[
+        'site-sidebar',
+        active && 'site-sidebar_active',
+      ]
+        .filter(className => className)
+        .join(' ')}
+    >
+      <nav className="site-sidebar-menu">
+        {links.map(link => (
+          <div className="site-sidebar-menu-item" key={link}>
+            <a href={`/${link.toLowerCase()}`} className="button">{link}</a>
+          </div>
+        ))}
+        <div className="site-sidebar-menu-item-space" />
+        {accountLinks.map(link => (
+          <div className="site-sidebar-menu-item" key={link}>
+            <a href={`/${link.toLowerCase()}`} className="button">{link}</a>
+          </div>
+        ))}
+        <div className="site-sidebar-menu-item-space" />
+        <button type="button" className="button" onClick={toggleTheme} style={{ width: '100%' }}>Toggle Theme</button>
+      </nav>
+    </div>
+  );
+};
 
 export default Sidebar;
