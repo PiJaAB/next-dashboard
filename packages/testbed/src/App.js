693334df66f32f9a4a9d7bff1be413f1ff653ec9
David <david.svartz@pija.se>
2019-11-21 10:37:34 +0100

    Sidebar states.

--- packages/testbed/src/App.js
+++ packages/testbed/src/App.js
@@ -1,3 +1,6 @@
+// TODO: Initial load and resize classes...
+// TODO: Save sidebar and color state in storage...
+
 import React, { useState, useEffect } from 'react';
 
 import Header from './components/layout/Header';
@@ -10,21 +13,49 @@ import './styles/index.scss';
 
 const App = () => {
   const [sidebarActive, setSidebarActive] = useState(false);
+  const [sidebarCompact, setSidebarCompact] = useState(false);
 
-  const toggleSidebar = () => {
+  const toggleSidebarActive = () => {
     setSidebarActive(!sidebarActive);
   };
 
+  const toggleSidebarCompact = () => {
+    setSidebarCompact(!sidebarCompact);
+  };
+
+  const noTransition = () => {
+    const { body } = document;
+    if (!body) return;
+    body.classList.add('body_resizing');
+    setTimeout(() => {
+      body.classList.remove('body_resizing');
+    });
+  };
+
   useEffect(() => {
+    setTimeout(() => {
+      document.getElementsByClassName('site')[0].classList.remove('site_initial-load');
+    }, 10);
+    const onResize = () => {
+      noTransition();
+    };
     if (window.innerWidth > 640) {
       setSidebarActive(true);
     }
+    window.addEventListener('resize', onResize);
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
   }, []);
 
   return (
-    <div className="site">
-      <Header toggleSidebar={toggleSidebar} />
-      <Sidebar active={sidebarActive} />
+    <div className="site site_initial-load">
+      <Header toggleSidebarActive={toggleSidebarActive} />
+      <Sidebar
+        sidebarActive={sidebarActive}
+        sidebarCompact={sidebarCompact}
+        toggleSidebarCompact={toggleSidebarCompact}
+      />
       <Content>
         <Start />
       </Content>
