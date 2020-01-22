e52be88f13ba7ef15488a7b51576783717106f6d
Niklas Nilsson <niklas.nilsson@pija.se>
2020-01-22 11:21:08 +0100

    Moving in sidebar into header and fix order

--- packages/next-dashboard/src/components/layout/index.js
+++ packages/next-dashboard/src/components/layout/index.js
@@ -2,7 +2,7 @@
 
 export { default as Content } from './Content';
 export { default as Footer } from './Footer';
-export { default as Header } from './Footer';
+export { default as Header } from './Header';
 export { default as DashboardLayout } from './Main';
 export { default as Sidebar } from './Sidebar';
 export { default as SiteMessages } from './SiteMessages';
