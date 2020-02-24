a5650f8e884353f1e80016145995d0442aa652c8
Alexander Lind <smulan300@gmail.com>
2020-02-24 09:42:47 +0100

    Updates for feature box
    Removed icon and icon background color from required props.
    Added new prop extraStyles.

--- packages/next-dashboard/src/components/FeatureBox.js
+++ packages/next-dashboard/src/components/FeatureBox.js
@@ -3,18 +3,22 @@ import React, { PureComponent } from 'react';
 import type { Statuses } from '../utils/types';
 
 export type Props = {
-  icon: string,
-  iconBackgroundColor: string,
+  icon?: string,
+  iconBackgroundColor?: string,
   label?: React$Node,
   value: string | number,
   footerComponent?: React$Node,
   status?: Statuses,
+  extraStyles?: { [string]: mixed },
 };
 
 export const defaultProps = {
+  icon: undefined,
+  iconBackgroundColor: undefined,
   label: undefined,
   status: undefined,
   footerComponent: undefined,
+  extraStyles: {},
 };
 
 export default class FeatureBox extends PureComponent<Props> {
@@ -28,19 +32,23 @@ export default class FeatureBox extends PureComponent<Props> {
       value,
       footerComponent,
       status,
+      extraStyles,
     } = this.props;
     return (
       <div
         className={['feature_box', status && `feature_box_status_${status}`]
           .filter(c => c)
           .join(' ')}
+        style={extraStyles}
       >
-        <div
-          className="feature_box_icon"
-          style={{ backgroundColor: iconBackgroundColor }}
-        >
-          <i className={`fa ${icon}`} />
-        </div>
+        {icon && iconBackgroundColor && (
+          <div
+            className="feature_box_icon"
+            style={{ backgroundColor: iconBackgroundColor }}
+          >
+            <i className={`fa ${icon}`} />
+          </div>
+        )}
         <div className="feature_box_label label margin-bottom-x1">{label}</div>
         <div className="feature_box_value margin-bottom-x1">
           <h2>{status === 'loading' ? 'Loading...' : value}</h2>
