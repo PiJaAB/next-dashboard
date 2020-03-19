b14ca90941699fd19b251c1df1ef97fa633a6f7a
Alexander Lind <smulan300@gmail.com>
2020-03-19 09:48:34 +0100

    fixed Unnecessary use of conditional expression for default assignment.

--- packages/next-dashboard/src/components/DropdownView.js
+++ packages/next-dashboard/src/components/DropdownView.js
@@ -72,7 +72,7 @@ const DropdownView = ({
       {isOpen && (
         // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
         <div
-          className={`${prefix}__content--container ${dropdownExtraClasses ??
+          className={`${prefix}__content--container ${dropdownExtraClasses ||
             null}`}
           onClick={ev => {
             ev.stopPropagation();
