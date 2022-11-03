70d3d2fd98ffa9bf38175d0a5650a5137d3896bf
Gökhan Alkacir <gokhan@pija.se>
2022-11-03 11:09:23 +0100

    style: spread _new_ focus color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,17 +14,9 @@ module.exports = {
         yellow: colors.amber,
         purple: colors.violet,
         focus: {
+          ...colors.indigo,
           DEFAULT: colors.indigo[500],
           dark: colors.indigo[300],
-          100: colors.indigo[100],
-          200: colors.indigo[200],
-          300: colors.indigo[300],
-          400: colors.indigo[400],
-          500: colors.indigo[500],
-          600: colors.indigo[600],
-          700: colors.indigo[700],
-          800: colors.indigo[800],
-          900: colors.indigo[900],
         },
         primary: {
           DEFAULT: colors.sky[500],
