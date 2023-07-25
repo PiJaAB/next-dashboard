43f0ce9b3794a1a5293ce9fa7e4de958e43b6086
Linn Dahlgren <linn.dahlgren@pija.se>
2023-07-25 23:42:07 +0200

    Omitted lockfile pruning

--- scripts/prune-lockfile.js
+++
@@ -1,7 +0,0 @@
-const yaml = require('js-yaml');
-const fs = require('fs');
-
-const document = yaml.load(fs.readFileSync('./pnpm-lock.yaml', 'utf-8'));
-document.importers = Object.fromEntries(Object.entries(document.importers).filter(([k]) => !k.startsWith('Projects/')));
-document.packages = Object.fromEntries(Object.entries(document.packages).filter(([,v]) => !v.$DEFUNCT_PROPERTY));
-fs.writeFileSync('./pnpm-lock.yaml', yaml.dump(document));
